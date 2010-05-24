1e9f8c2e7988e08b2bdbd5904afaf5219d78db9d
metvolt220 <metvolt220@3db799bf-e753-161e-daf0-3aab31b4776b>
2010-05-24 16:50:44 +0000

    Добавлена возможность уазания базового адреса модуля. Добавленга возможность воспользоваться автолоадом на множестве папок. Добавлено хэширование автолоада.

--- VoltCore_Moscow.php
+++ VoltCore_Moscow.php
@@ -5,7 +5,7 @@
 	 * @author Костин Алексей Васильевич aka Volt(220)
 	 * @copyright Copyright (c) 2010, Костин Алексей Васильевич
 	 * @license http://www.gnu.org/licenses/gpl-3.0.html GNU Public License
- 	 * @version 1.$Revision$
+ 	 * @version 1.0
 	 * @package voltcore
 	 */
 
